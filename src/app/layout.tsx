577102e7cd70f443215813ba87a509d7eaedbc20
Mrsol_123 <mrrsol.it@gmail.com>
2025-10-30 16:47:12 +0700

    add loading :

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { url } from "inspector";
 import { URL } from "url";
+import { Suspense } from "react";
+import Loading from "./loading";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -42,7 +44,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+       <Suspense fallback ={<Loading/>} >
         {children}
+       </Suspense>
       </body>
     </html>
   );
